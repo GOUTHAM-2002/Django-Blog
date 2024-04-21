41e5abf5c0679f76e13f77bfd253c9369d526095
goutham <goutham3336@gmail.com>
2024-04-22 00:34:51 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('',views.register,name='users-register'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="users-login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name="users-logout"),
+    path('profile/',views.profile,name="users-profile")
+
+]
