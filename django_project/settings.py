81eb578ffff07a7006612f62638b46dc18c215a7
goutham <goutham3336@gmail.com>
2024-04-23 01:16:42 +0530

    FInalised app

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,3 +135,10 @@ LOGIN_URL = 'users-login'
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL="/media/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'meowbow22@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('GMAIL_PASSWORD')
