51e1a6f56ab2e690f63dcf6933f1f371f69bfac7
goutham <goutham3336@gmail.com>
2024-04-23 23:25:37 +0530

    f

--- users/apps.py
+++ users/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
+        import sys
+        print(sys.path)  # Print out the system paths to debug path issues
         import users.signals
-
